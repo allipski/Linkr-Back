32c4687fec7c3d4e8600449323057232acc39932
Brunopjunq <bruno_junq@poli.ufrj.br>
2022-10-20 09:51:57 -0300

    Add

--- src/controllers/hashtagController.js
+++ src/controllers/hashtagController.js
@@ -2,7 +2,7 @@ import { connection } from "../database/database.js";
 
 export async function getTrendingHashtags(req,res) {
     try {
-        const hashtags = await connection.query(`SELECT * FROM hashtags`)
+        //const hashtags = await connection.query(`SELECT * FROM hashtags`)
     } catch (error) {
         console.log(error);
         return res.sendStatus(500);
