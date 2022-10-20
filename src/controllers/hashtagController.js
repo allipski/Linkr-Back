d9defb9d4064cffdba7507824617061f1378b5c1
Brunopjunq <bruno_junq@poli.ufrj.br>
2022-10-20 09:35:54 -0300

    Add1

---
+++ src/controllers/hashtagController.js
@@ -0,0 +1,10 @@
+import { connection } from "../database/database.js";
+
+export async function getTrendingHashtags(req,res) {
+    try {
+        const hashtags = await connection.query(`SELECT * FROM hashtags`)
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+}
