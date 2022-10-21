700112766da0cc2d5e804b478a4b72aeae215a6d
Edson0012 <Edsonewertonrs1@gmail.com>
2022-10-21 19:07:20 -0300

    finalizado back de likes

--- src/routers/likeRoutes.js
+++ src/routers/likeRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { postLikeFunction } from "../controllers/likeController.js";
+import { deleteLike, getLikeCount, postLikeFunction } from "../controllers/likeController.js";
 import { validateToken }  from "../middleware/validateToken.js"
 
 
 const router = Router();
 
 router.post('/like', validateToken, postLikeFunction);
+router.get('/like/:id', validateToken, getLikeCount);
+router.delete('/like/:id', validateToken, deleteLike)
 
 export default router;
