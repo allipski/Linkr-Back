87ba1566d6d8885624f3eb03ccff6d536e2ccfa1
Edson0012 <Edsonewertonrs1@gmail.com>
2022-10-23 15:50:52 -0300

    delete post Back

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -3,3 +3,31 @@ import * as postsRepository from "../repositories/postsRepository.js";
 export async function publishPost(req, res) {
   const { userId, url, description } = req.body;
 }
+
+export async function deletePost(req , res){
+  const { id } = req.params;
+  const { user } = res.locals;
+  console.log(user)
+  try{
+
+    if(!req.params){
+      return res.sendStatus(404);
+    }
+
+    const postId = id;
+
+    const{rows: postUser} = postsRepository.verifyUserPost({user, postId})
+    console.log(postUser)
+
+    if(!postUser[0]){
+      return res.sendStatus(404);
+    }
+    
+    postsRepository.deleteUser({postId});
+
+    return res.sendStatus(204);
+  }catch(err){
+    console.log(err)
+    return res.status(500).send("server error")
+  }
+}
